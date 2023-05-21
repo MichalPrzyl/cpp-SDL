3848f9e4babd226487faaeb2b1a0c38b01dd0eef
michalPrzyl <michal.przyl@gmail.com>
2023-05-21 18:18:19 +0200

    changing colors after every wall hit

--- window.cpp
+++ window.cpp
@@ -1,6 +1,19 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
+#include <cstdlib>
+
+// random number generator
+int generate_random_number(int min, int max){
+	int return_value = rand() % max + 1;
+	return return_value;
+}
+
+// generating 3 element array of color rbg values
+int * get_new_color(){
+	int new_color[3] = {1,2,3};
+	return new int[3] {new_color[0], new_color[1], new_color[2]};
+}
 
 int main(){
 	printf("Hello world\n");
@@ -12,6 +25,8 @@ int main(){
 	int change_in_position = 5;
 	int change_in_y_position = 3;
 	int delay = 20;
+	int color[3] = {255, 0, 255};
+	//int* color;
 
 	window = SDL_CreateWindow("MP SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
 	//SDL_Delay(3000);
@@ -30,14 +45,16 @@ int main(){
 	// direction of moving
 	bool going_right = true;
 	bool going_down = true;
-
+	
 	// main loop
 	while (!close){
 		SDL_Event event;
+		
 
 		// renderer
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 0, color[0], color[1], color[2]);
 		SDL_RenderClear(renderer);
 		
 		// trying to draw a rectangle
@@ -63,18 +80,31 @@ int main(){
 		}
 
 		// changing direction
-		if (x_position >= width - square_width)
+		if (x_position >= width - square_width){
 			going_right = false;
-		if (x_position < 0)
+		}
+		if (x_position < 0){
 			going_right = true;
+		}
 		// down and up
-		if (y_position >= height - square_height)
+		if (y_position >= height - square_height){
 			going_down = false;
-		if (y_position < 0)
+		}
+		if (y_position < 0){
 			going_down = true;
+		}
+		if (x_position >= width - square_width ||
+				x_position < 0 ||
+				y_position >= height - square_height ||
+				y_position < 0){
+			color[0] = generate_random_number(0, 255);
+			color[1] = generate_random_number(0, 255);
+			color[2] = generate_random_number(0, 255);
+		}
 
 		SDL_Delay(delay);
 		SDL_SetRenderDrawColor(renderer, 0, 150, 100, 210);
+		//SDL_SetRenderDrawColor(renderer, 0, color[0], color[1], color[2]);
 
 		SDL_RenderDrawRect(renderer, &rectangle);
 
